4bb8da1f1f93555d2546f7bdb25141d2e5a5e813
Brian Stasiukaitis <brianstas@gmail.com>
2023-10-02 13:26:38 -0400

    built out basic functionality
    Basic functionality is complete, though it needs to be edited to fix this specific project. Still, routers and controllers now exist, so moving in the right direction.

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -6,4 +6,78 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /dishes handlers needed to make the tests pass
+function create(req, res) {
+  const { data: { name, description, price, image_url } = {} } = req.body;
+  const newDish = {
+    id: nextId,
+    name,
+    description,
+    price,
+    image_url
+  };
+  dishes.push(newDish);
+  res.status(201).json({ data: newDish });
+}
+
+function destroy(req, res) {
+  const { dishId } = req.params;
+  const index = dishes.findIndex((dish) => dish.id === Number(dishId));
+  if (index > -1) {
+    dishes.splice(index, 1);
+  }
+  res.sendStatus(204);
+}
+
+function hasText(req, res, next) {
+  const { data: { text } = {} } = req.body;
+
+  if (text) {
+    return next();
+  }
+  next({ status: 400, message: "A 'text' property is required." });
+}
+
+function list(req, res) {
+  res.json({ data: dishes });
+}
+
+function dishExists(req, res, next) {
+  const dishId = Number(req.params.dishId);
+  const foundDish = dishes.find((dish) => dish.id === dishId);
+  if (foundDish) {
+    return next();
+  }
+  next({
+    status: 404,
+    message: `Dish id not found: ${req.params.dishId}`,
+  });
+}
+
+function read(req, res) {
+  const dishId = Number(req.params.dishId);
+  const foundDish = dishes.find((dish) => (dish.id = dishId));
+  res.json({ data: foundDish });
+}
+
+function update(req, res) {
+  const dishId = Number(req.params.dishId);
+  const foundDish = dishes.find((dish) => dish.id === dishId);
+
+  const { data: { name, description, price, image_url } = {} } = req.body;
+
+  foundDish.name = name;
+  foundDish.description = description;
+  foundDish.price = price;
+  foundDish.image_url = image_url;
+
+  res.json({ data: foundDish });
+}
+
+module.exports = {
+  create: [hasText, create],
+  list,
+  read: [dishExists, read],
+  update: [dishExists, hasText, update],
+  delete: destroy,
+  dishExists,
+};
--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -5,5 +5,96 @@ const orders = require(path.resolve("src/data/orders-data"));
 
 // Use this function to assigh ID's when necessary
 const nextId = require("../utils/nextId");
+const validatorFor = require('../utils/validatorFor.js');
 
-// TODO: Implement the /orders handlers needed to make the tests pass
+function validateBodyExists(req, res, next) {
+    if (req.body.data) {
+      next();
+    } else {
+      next({
+        message: 'request.body must include data',
+        status: 400
+      })
+    }
+  }
+
+function create(req, res) {
+    const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
+    const newOrder = {
+      id: nextId,
+      deliverTo,
+      mobileNumber,
+      status,
+      dishes
+    };
+    orders.push(newOrder);
+    res.status(201).json({ data: newOrder });
+  }
+  
+  function destroy(req, res) {
+    const { orderId } = req.params;
+    const index = orders.findIndex((order) => order.id === Number(orderId));
+    if (index > -1) {
+      orders.splice(index, 1);
+    }
+    res.sendStatus(204);
+  }
+  
+  function list(req, res) {
+    res.json({ data: orders });
+  }
+  
+  function orderExists(req, res, next) {
+    const orderId = Number(req.params.orderId);
+    const foundOrder = orders.find((order) => order.id === orderId);
+    if (foundOrder) {
+      return next();
+    }
+    next({
+      status: 404,
+      message: `Order id not found: ${req.params.orderId}`,
+    });
+  }
+  
+  function read(req, res) {
+    const orderId = Number(req.params.orderId);
+    const foundOrder = orders.find((order) => (order.id = orderId));
+    res.json({ data: foundOrder });
+  }
+  
+  function update(req, res) {
+    const orderId = Number(req.params.dishId);
+    const foundOrder = prders.find((order) => order.id === orderId);
+  
+    const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
+  
+    foundOrder.deliverTo = deliverTo;
+    foundOrder.mobileNumber = mobileNumber;
+    foundOrder.status = status;
+    foundOrder.dishes = dishes;
+  
+    res.json({ data: foundOrder });
+  }
+  
+  module.exports = {
+    list,
+    create: [
+      validateBodyExists,
+      validatorFor('deliverTo'),
+      validatorFor('mobileNumber'),
+      validatorFor('status'),
+      validatorFor('dishes'),
+      create
+    ],
+    read: [orderExists, read],
+    update: [
+      orderExists,
+      validateBodyExists,
+      validatorFor('deliverTo'),
+      validatorFor('mobileNumber'),
+      validatorFor('status'),
+      validatorFor('dishes'),
+      update
+    ],
+    destroy: [orderExists, destroy]
+  }
